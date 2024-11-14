4493e4de8b9b5f04ffa5a3149091aab71a9c15e1
FreelyTian <osk9ehdms01@gmail.com>
2024-11-13 22:02:49 -0300

    initial

---
+++ lib/DesignSystem/Shared/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// Cores de texto
+const Color primaryText = Color(0xff2D2D41);
+const Color alternativeText = Color(0xff676578);
+const Color mutedText = Color(0xff908F9D);
+const Color secondaryText = Color(0xffF4F4F6);
+const Color destructive = Color(0xffF48780);
+const Color success = Color(0xff93CB80);
+
+
+// Cores de fundo
+const Color primaryBackground = Color.fromARGB(255, 244, 244, 246);
+const Color primaryForeground = Color(0xFFE7E6EE);
+
+// De resto a maioria são gradientes, que são um pouco mais complexos de se definir
